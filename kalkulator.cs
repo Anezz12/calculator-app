62087e733302343082a77040907945ac7f7831c1
Anezz12 <harsenaargrtya1@gmail.com>
2023-03-13 16:52:34 +0700

    instalisasi project

--- kalkulator.cs
+++ kalkulator.cs
@@ -2,6 +2,15 @@ class Calculator
 {
 	static void Main(string)[] args)
 	{
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2} ", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan: {0} + {1} = {2} ", a, b, Pengurangan(a, b));
+		
+		Console.WriteLine("\nTekan sembarang key untuuk keluar");
+		Console.ReadKey();
 		
 	}
+	static int Penambahan
 }
